4d61cbaeadc7ef624ae18f8ad5ae322a4c89a5d6
Oleg Golosovskiy <ogolosovsky@intermedia.net>
2019-01-15 16:19:53 +0300

    update test

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ An OTP application
 Build
 -----
 
-    $ rebar3 shell
-
-
+$ rebar3 shell
+1> mnesia_test_gen:write_test(30000).
+write (one index for 30000 records): 1.309399 seconds
+write (two indexes for 30000 records): 1.443878 seconds
+ok
+2> mnesia_test_gen:update_test(30000).
+update(one index for 30000 records): 1.350957 seconds
+update(two indexes for 30000 records): 1.475005 seconds
+ok
--- apps/mnesia_test/src/mnesia_test_gen.erl
+++ apps/mnesia_test/src/mnesia_test_gen.erl
@@ -23,6 +23,16 @@
     code_change/3]).
 
 -define(SERVER, ?MODULE).
+%%-compile([debug_info, export_all]).
+
+-export([
+            write1/1,
+            write2/1,
+            update1/1,
+            update2/1,
+            write_test/1,
+            update_test/1
+        ]).
 
 -include("common.hrl").
 
@@ -43,27 +53,94 @@
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
+
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
 
 
--record(test_record1, {us = {<<"">>, <<"">>} :: {binary(), binary()},  info = #{} :: map() | '_'}).
-
+-record(test_record1, {key1 = <<"">> :: binary(), key2 = <<"">> :: binary(),  data = #{} :: map() | '_'}).
+-record(test_record2, {key1 = <<"">> :: binary(), key2 = <<"">> :: binary(),  data = #{} :: map() | '_'}).
 
 -spec(init(Args :: term()) ->
     {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
     {stop, Reason :: term()} | ignore).
 init([]) ->
     ?TRACE(),
-
-    mnesia:create_schema([node()]), %% {error,{nonode@nohost,{already_exists,nonode@nohost}}}
+    mnesia:create_schema([nodes()]),
     mnesia:start(),
-    mnesia:delete_table(test_table),
-    Res = mnesia:create_table(test_table, [ {disc_copies, [node()]}, {attributes, record_info(fields,test_record1)} ]),
-    lager:debug("Created ~p~n", [Res]),
-           %% {index, [#test_record.field1]},
+    timer:sleep(500),
+
+
+    mnesia:delete_table(test_record1),
+    mnesia:delete_table(test_record2),
+    {atomic, ok} = mnesia:create_table(test_record1, [  {disc_copies, [node()]}, {attributes, record_info(fields,test_record1)} ]),
+    {atomic, ok} = mnesia:create_table(test_record2, [ {disc_copies, [node()]}, {index, [#test_record2.key2]}, {attributes, record_info(fields,test_record2)} ]),
     {ok, #state{}}.
+
+
+write1(L) ->
+    F = fun() ->
+            Test1 =  #test_record1{key1 = base64:encode(crypto:strong_rand_bytes(6)), key2 =base64:encode(crypto:strong_rand_bytes(6)), data = #{}},
+            mnesia:write(Test1)
+        end,
+    [{atomic,ok} = mnesia:transaction(F) || _ <- L],
+    ok.
+
+write2(L) ->
+    F = fun() ->
+        Test1 =  #test_record2{key1 = base64:encode(crypto:strong_rand_bytes(6)), key2 =base64:encode(crypto:strong_rand_bytes(6)), data = #{}},
+        mnesia:write(Test1)
+        end,
+    [{atomic,ok} = mnesia:transaction(F) || _ <- L],
+    ok.
+
+write_test(Count) ->
+    L = lists:seq(1, Count),
+
+    {Occ1, _} =  timer:tc(mnesia_test_gen, write1, [L]),
+    io:format("write (one index for ~p records): ~w seconds ~n", [Count, Occ1 / 1000000]),
+
+    {Occ2, _} =  timer:tc(mnesia_test_gen, write2, [L]),
+    io:format("write (two indexes for ~p records): ~w seconds ~n", [Count, Occ2 / 1000000]),
+
+    ok.
+
+
+update1(L) ->
+     F = fun() ->
+         Key = <<"wfOZTPx7">>,
+         NewRecord = case mnesia:select(test_record1, [{#test_record1{key1 = Key, _ = '_', _ = '_'}, [], ['$_']}]) of
+                       [Record] -> Record;
+                       [] -> #test_record1{key1 = Key, key2 =base64:encode(crypto:strong_rand_bytes(6)), data = #{}}
+                   end,
+         mnesia:write(NewRecord)
+         end,
+    [{atomic,ok} = mnesia:transaction(F) || _ <- L].
+
+update2(L) ->
+    F = fun() ->
+        Key = <<"wfOZTPx8">>,
+        NewRecord = case mnesia:select(test_record2, [{#test_record2{key1 =  '_', key2 = Key, _ = '_'}, [], ['$_']}]) of
+                        [Record] -> Record;
+                        [] -> #test_record2{key1 = base64:encode(crypto:strong_rand_bytes(6)), key2 = Key, data = #{}}
+                    end,
+        mnesia:write(NewRecord)
+        end,
+    [{atomic,ok} = mnesia:transaction(F) || _ <- L].
+
+update_test(Count) ->
+    L = lists:seq(1, Count),
+
+    {Occ1, _} =  timer:tc(mnesia_test_gen, write1, [L]),
+    io:format("update(one index for ~p records): ~w seconds ~n", [Count, Occ1 / 1000000]),
+
+    {Occ2, _} =  timer:tc(mnesia_test_gen, write2, [L]),
+    io:format("update(two indexes for ~p records): ~w seconds ~n", [Count, Occ2 / 1000000]),
+
+    ok.
+
+
 
 
 -spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
